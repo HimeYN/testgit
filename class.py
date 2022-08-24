4cf0b434327b51ead51d35102a8ee339ae158aae
HimeYN <gokudera.hime@outlook.fr>
2022-08-24 11:14:58 +0200

    ajout test

--- class.py
+++ class.py
@@ -1,5 +1,9 @@
 class lol():
     def __init__(self):
         self.lel="lel"
+<<<<<<< HEAD
         self.attr=False
+=======
+        self.test="test"
+>>>>>>> 0ef5664 (ajout test)
         self.isTrue=True
