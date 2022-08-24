2880dd56f43af84d5606d879034072e6aa81b23d
HimeYN <gokudera.hime@outlook.fr>
2022-08-24 11:28:00 +0200

    changment

--- class.py
+++ class.py
@@ -1,13 +1,7 @@
 class lol():
     def __init__(self):
         self.lel="lel"
-<<<<<<< HEAD
         self.attr=False
-<<<<<<< HEAD
-=======
         self.test="test"
->>>>>>> 0ef5664 (ajout test)
-=======
         self.age=2
->>>>>>> 37aff14 (changment)
         self.isTrue=True
